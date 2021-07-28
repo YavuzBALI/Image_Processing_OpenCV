a79eed006bbbeaf751b6a518a64e7d7233b7b86c
Yavuz BALI <84620286+YavuzBALI@users.noreply.github.com>
2021-07-28 23:14:35 +0300

    Update and rename Resize.py to Resize_Frame.py

--- Resize_Frame.py
+++ Resize_Frame.py
@@ -1,20 +1,18 @@
 import cv2
 print(cv2.__version__)
-
+#Capture frame from camera
 cam=cv2.VideoCapture(0)
 while True:
+    #Assign frame to variable
     ret, frame = cam.read()
+    #Resize frame to 320 width 240 height
     frame2=cv2.resize(frame,(320,240))
     cv2.imshow('nanoCam',frame2)
     cv2.moveWindow('nanoCam',0,0)
 
-    small=cv2.resize(frame,(320,240))
-    gray=cv2.cvtColor(small,cv2.COLOR_BGR2HSV)
-    cv2.moveWindow('BW',320,0)
-    cv2.imshow('BW',gray)
-
-
+    #To exit, input is taken from the keyboard.
     if cv2.waitKey(1)==ord('q'):
         break
+        
 cam.release()
 cv2.destroyAllWindows()
