aaab036c0933144f0aa3994fbb58471799bf4831
Yavuz BALI <84620286+YavuzBALI@users.noreply.github.com>
2021-07-28 22:11:15 +0300

    Update and rename KAmer_denemee.py to Camera_testing.py

--- Camera_testing.py
+++ Camera_testing.py
@@ -1,18 +1,20 @@
 import cv2
 print(cv2.__version__)
 
+#Capture frame from camera
 cam=cv2.VideoCapture(0)
 while True:
+    #Assign frame to variable
     ret, frame = cam.read()
+    #Printing the image on the screen 
     cv2.imshow('nanoCam',frame)
+    #Positioning your image on the screen 
     cv2.moveWindow('nanoCam',0,200)
-    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    cv2.imshow('graywindow',gray)
-    cv2.moveWindow('graywindow',500,200)
-
-
-
+    
+    #Getting input from the keyboard so exit the code
     if cv2.waitKey(1)==ord('q'):
         break
+#Release the camera
 cam.release()
+#Erase the screen
 cv2.destroyAllWindows()
